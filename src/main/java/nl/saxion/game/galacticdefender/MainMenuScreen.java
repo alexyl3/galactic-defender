448b4f9f68c3e849aff6af7a4afdec3433f2d67b
sheetal <sheetalmacharla@gmail.com>
2025-11-20 13:13:37 +0100

    menu screen

--- src/main/java/nl/saxion/game/galacticdefender/MainMenuScreen.java
+++ src/main/java/nl/saxion/game/galacticdefender/MainMenuScreen.java
@@ -12,6 +12,11 @@ public class MainMenuScreen extends ScalableGameScreen {
     @Override
     public void show() {
         GameApp.addFont("basic", "fonts/basic.ttf", 100);
+        GameApp.addFont("basic", "fonts/basic.ttf", 100);
+        GameApp.addFont("FreePixel", "fonts/FreePixel.ttf", 70);
+        GameApp.addTexture("background","textures/background.png");
+        GameApp.addTexture("play_button", "textures/play_button.png");
+        GameApp.addTexture("Rectangle_box","textures/Rectangle_box.png");
     }
 
     @Override
@@ -26,8 +31,23 @@ public class MainMenuScreen extends ScalableGameScreen {
         // Render the main menu
         GameApp.clearScreen("black");
         GameApp.startSpriteRendering();
-        GameApp.drawTextCentered("basic", "Start Game (press enter)", getWorldWidth()/2, getWorldHeight()/2, "amber-500");
+        GameApp.drawTextureCentered("background",getWorldWidth()/2f,getWorldHeight()/2f);
+        GameApp.drawTextureCentered("Rectangle_box",getWorldWidth()/2f,getWorldHeight()/2f+40,750,500);
+
+        float btnX = getWorldWidth() / 2f - 40;   // button width ~160
+        float btnY = getWorldHeight() / 2f - 120;  // lower
+
+        GameApp.drawTexture("play_button", btnX, btnY, 120, 80);
         GameApp.endSpriteRendering();
+
+        GameApp.startSpriteRendering();
+        String title = "FreePixel";
+        float textX = getWorldWidth()/2f-300;
+        float textY = getWorldHeight()/2f+20;
+        GameApp.drawText(title,"Galactic Defender",textX,textY,"white");
+        GameApp.endSpriteRendering();
+        if (GameApp.isKeyPressed(Input.Keys.ESCAPE)){
+            GameApp.switchScreen("YourGameScreen");}
     }
 
     @Override
