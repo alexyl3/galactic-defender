64dd947fe53e540e502c4e143b4258d692fc3e6e
sheetal <sheetalmacharla@gmail.com>
2025-11-24 17:26:43 +0100

    menu screen

--- src/main/java/nl/saxion/game/galacticdefender/GameScreen.java
+++ src/main/java/nl/saxion/game/galacticdefender/GameScreen.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GameScreen extends ScalableGameScreen {
+    int[] AsteriodX = new int[6];
+    int[] AsteriodY = new int[6];
     public static final int BG_SPEED = 700;
     public static final int SPACESHIP_SPEED = 300;
     public static final int SPACESHIP_SIZE = 80;
@@ -17,6 +19,7 @@ public class GameScreen extends ScalableGameScreen {
     float bullet_timer = 0;
     SpaceShip player;
     ArrayList<Float> bullets = new ArrayList<>();
+
     public GameScreen() {
         super(500, 800);
     }
@@ -31,6 +34,13 @@ public class GameScreen extends ScalableGameScreen {
         player = new SpaceShip();
         player.x = getWorldWidth() / 2;
         player.y = 0;
+        int topMin = (int) (GameApp.getWorldHeight() - 200);
+        int topMax = (int) GameApp.getWorldHeight();
+
+        for (int index = 0; index < 5; index++) {
+            AsteriodX[index] = GameApp.randomInt(0, (int) GameApp.getWorldWidth());
+            AsteriodY[index] = GameApp.randomInt(topMin, topMax);
+        }
     }
 
     @Override
@@ -65,6 +75,9 @@ public class GameScreen extends ScalableGameScreen {
                 float bulletY = GameApp.updateInterpolator("bullet" + i) * delta * BULLET_SPEED + GameApp.getTextureHeight("spaceship");
                 GameApp.drawTexture("shot", bullets.get(i), bulletY);
             }
+        }
+        for (int i = 0; i < 6; i++) {
+            GameApp.drawTexture("asteriod", AsteriodX[i] - 50, AsteriodY[i] - 70, 50, 30);
         }
         GameApp.endSpriteRendering();
 
--- src/main/java/nl/saxion/game/galacticdefender/MainMenuScreen.java
+++ src/main/java/nl/saxion/game/galacticdefender/MainMenuScreen.java
@@ -48,6 +48,8 @@ public class MainMenuScreen extends ScalableGameScreen {
         GameApp.endSpriteRendering();
         if (GameApp.isKeyPressed(Input.Keys.ESCAPE)){
             GameApp.switchScreen("YourGameScreen");}
+
+
     }
 
     @Override
